29e2fb8ec09c8be7adf9a7ba0b17ebb2b8ac51d7
Rod Yager <rod@yager.net.au>
2025-03-04 16:19:44 +1100

    Initial Commit

---
+++ DataScanner/DataScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataScannerApp.swift
+//  DataScanner
+//
+//  Created by Rod Yager on 4/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct DataScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
